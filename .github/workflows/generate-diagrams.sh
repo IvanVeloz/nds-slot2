5261577ca9c9f179e2fd9cbb9d90c5ba718664a9
Ivan Veloz <devel@ivanveloz.com>
2025-02-23 15:57:59 -0500

    Change ownership for deployment

--- .github/workflows/generate-diagrams.sh
+++ .github/workflows/generate-diagrams.sh
@@ -8,5 +8,23 @@ $WDCLI -f png *.json
 $WDCLI -f svg *.json
 mv *.png diagrams
 mv *.svg diagrams
+echo "Diagrams generated:"
 ls diagrams
 
+CURRENT_USER=$(whoami)
+CURRENT_GROUP=$(id -g -n)
+echo "Current user: $CURRENT_USER"
+echo "Current group: $CURRENT_GROUP"
+
+pushd diagrams
+for f in *.png; do
+  echo "Fixing ownership for $f"
+  chown $CURRENT_USER:$CURRENT_GROUP $f
+done
+for f in *.svg; do
+  echo "Fixing ownership for $f"
+  chown $CURRENT_USER:$CURRENT_GROUP $f
+done
+popd
+
+echo "Done"
